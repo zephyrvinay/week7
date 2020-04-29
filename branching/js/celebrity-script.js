32ff194d9e8153ab3f3f4f0e351a4baa815e11cc
vinay prabhushankar <zephyr.vinay@gmail.com>
2020-04-28 20:09:18 -0700

    week 7 celebrity practice

---
+++ branching/js/celebrity-script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("form#celebrity").submit(function(event){
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val(); 
+     
+    if (gender === 'male' && age > 26) {
+      $("#match").text("jessica alba");
+      $("#result").show();
+    } else if (gender === 'female' && age > 26){
+      $("#match").text("Rick Sanchez");
+      $("#result").show();
+    } else {
+      $("#match").text("Morty");
+      $("#result").show();
+    }
+    event.preventDefault();
+
+
+  });
+});
