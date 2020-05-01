f8efb2fba7d8fda7a8214a6e60e7871332a98dbd
vinay prabhushankar <zephyr.vinay@gmail.com>
2020-04-30 20:28:36 -0700

    loops to figure out the character

--- character/js/scripts.js
+++ character/js/scripts.js
@@ -3,5 +3,18 @@ $(document).ready(function() {
     event.preventDefault();
     var name = ($("#name1")).val();
     console.log(name);
+    var eveningInput = $("input:radio[name=activity]:checked").val();
+    console.log(eveningInput);
+
+    if (eveningInput === "chair") { 
+      var result = "Ron Swanson";
+    } else if (eveningInput === "club"){
+      var result = "Tom haverford";
+    } else if (eveningInput === "binders"){
+      var result = "Leslie Knope";
+    }  
+      
+    $("#output").text(result);
+
   });
 })    
